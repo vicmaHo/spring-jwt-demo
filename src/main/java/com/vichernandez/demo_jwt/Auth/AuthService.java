b68b5fabfbc7e198c356184bbc46f6520ffb0ef8
Victor Hernandez <victormanu968@gmail.com>
2024-05-29 21:19:50 -0500

    login implemented

--- src/main/java/com/vichernandez/demo_jwt/Auth/AuthService.java
+++ src/main/java/com/vichernandez/demo_jwt/Auth/AuthService.java
@@ -1,6 +1,9 @@
 
 package com.vichernandez.demo_jwt.Auth;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,9 +24,20 @@ class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+
+
     public AuthResponse login(LoginRequest request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+
+        String token = jwtService.getToken(user);
+
+        return AuthResponse.builder()
+            .token(token)
+            .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
